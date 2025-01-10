430232e31434c79b6bee78c6bfb5bce9984290e5
Bikram Majhi <iiserkbikram@gmail.com>
2025-01-10 23:39:13 +0530

    Diffusion skeleton and TimeEmbedding

---
+++ diffusion.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from attention import SelfAttention, CrossAttention
+
+class TimeEmbedding(nn.Module):
+    def __init__(self, n_embed: int):
+        super().__init__()
+        self.linear_1 = nn.Linear(n_embed, 4 * n_embed)
+        self.linear_2 = nn.Linear(4 * n_embed, 4 * n_embed)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # (1, 320)
+        x = self.linear_1(x)
+        x = F.silu(x)
+        x = self.linear_2(x)
+        # (1, 1280)
+        return x
+
+
+class Diffusion(nn.Module):
+    def __init__(self):
+        self.time_embedding = TimeEmbedding(320)
+        self.unet = UNET()
+        self.final = UNET_OutputLayer(320, 4)
+    
+    def forward(self, latent: torch.Tensor, context: torch.Tensor, time: torch.Tensor):
+        # latent: (Batch_size, 4, Height / 8, Width / 8)
+        # context: (Batch_size, Seq_len, Dim)
+        # time: (1, 320)
+        
+        # (1, 320) -> (1, 1280)
+        time = self.time_embedding(time)
+        
+        # (Batch, 4, Height / 8, Width / 8) -> (Batch, 320, Height / 8, Width / 8)
+        output = self.unet(latent, context, time)
+        
+        # (Batch, 320, Height / 8, Width / 8) -> (Batch, 4, Height / 8, Width / 8)
+        output = self.final(output)
+        
+        #(Batch, 4, Height / 8, Width / 8)
+        return output
